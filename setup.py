9143198a4fd67a7917a5292f47d1909ece450995
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-10 17:49:43 +0200

    Don't forget setup.* files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Hello Bluenove',
+    version='1.0',
+    description='Simple flask app',
+    author='Bluenove',
+    author_email='',
+    url='https://github.com/bluenove/flask-example',
+    packages=find_packages()
+)
