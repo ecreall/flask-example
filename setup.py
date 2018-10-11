200b4c1629f1f3ef4ccd7a7d5ac91b7b92299a4b
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-11 14:57:50 +0200

    Try again

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hellobluenove',
-    version='1.3',
+    version='1.4',
     description='Simple flask app',
     author='Bluenove',
     author_email='',
