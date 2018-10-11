6653ca03a966d15d007e49bd1644b79f5ba5ce7a
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-11 14:50:12 +0200

    More ssh stuff

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hellobluenove',
-    version='1.2',
+    version='1.3',
     description='Simple flask app',
     author='Bluenove',
     author_email='',
