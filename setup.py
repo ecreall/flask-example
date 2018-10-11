20f4bbad1e34f302c1db03e5d06a6df1b7ee6433
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-11 12:15:29 +0200

    Upgrade version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Hello Bluenove',
-    version='1.0',
+    version='1.1',
     description='Simple flask app',
     author='Bluenove',
     author_email='',
