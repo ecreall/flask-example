ee49583b7cffe1cba2722add4fee86c5bd1f9622
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-11 16:56:52 +0200

    Remove venv before deploy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hellobluenove',
-    version='1.6',
+    version='1.7',
     description='Simple flask app',
     author='Bluenove',
     author_email='',
